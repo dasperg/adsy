c26aab7ab61280c5874210f80f09814769f5ae5d
dasperg <dasperg@gmail.com>
2019-02-08 17:03:24 +0100

    Fix home route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'ExpenseController@index')->name('home');
 
 Route::resource('expense', 'ExpenseController')->middleware('auth');
